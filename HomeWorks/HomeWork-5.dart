f679de90bec1ba6a5ac273565480ebbee50900ba
Muharrem GEDİK <39222515+gdkmuharrem@users.noreply.github.com>
2023-03-14 14:46:36 +0300

    Update HomeWork-5.dart

--- HomeWorks/HomeWork-5.dart
+++ HomeWorks/HomeWork-5.dart
@@ -5,3 +5,20 @@
       [Formül : n--> kenar sayısı  : (n-2)*180 ]
   ----------
 */
+import 'dart:io';
+
+void main(){
+
+  print("--------------------------------------------------------");
+  print("Üçgen İç Açılar Toplamını Hesaplama Botuna Hoş Geldiniz.");
+  print("--------------------------------------------------------");
+  print("İç açıları toplamını öğrenmek istediğiniz üçgenin kenar sayısını giriniz.");
+  int kenar = int.parse(stdin.readLineSync()!);
+  print(icAci(kenar));
+
+}
+
+int icAci(int kenar){
+  int sonuc = (kenar-2)*180;
+  return sonuc;
+}
