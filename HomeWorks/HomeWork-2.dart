2424f58e425da36f58050472f361f3a40915bfb8
Muharrem GEDİK <39222515+gdkmuharrem@users.noreply.github.com>
2023-03-14 14:10:42 +0300

    Create HomeWork-2.dart

---
+++ HomeWorks/HomeWork-2.dart
@@ -0,0 +1,22 @@
+/*
+  HomeWork-2
+  ----------
+  2.Kenarları parametre olarak girilen ve dikdörtgenin çevresini hesaplayan bir method yazınız.
+  ----------
+*/
+import 'dart:io';
+
+void main (){
+  print("Dikdörtgen Çevre Hesabı Botumuza Hoş Geldiniz.");
+  print("Lütfen çevresini hesaplamak istediğiniz dikdörtgenin kenarlarını giriniz.");
+  print("Uzun kenar : ");
+  int uznknr = int.parse(stdin.readLineSync()!);
+  print("Kısa kenar : ");
+  int ksknr = int.parse(stdin.readLineSync()!);
+  cevre(uznknr,ksknr);
+}
+
+void cevre(int uznknr, int ksknr){
+  int sonuc = uznknr*2+ksknr*2;
+  print("Dikdörtgenin Çevresi : $sonuc");
+}
