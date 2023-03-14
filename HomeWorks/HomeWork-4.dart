5d81535e0dab5615ae065bd70e61ba254a8986f6
Muharrem GEDİK <39222515+gdkmuharrem@users.noreply.github.com>
2023-03-14 14:31:10 +0300

    Update HomeWork-4.dart

--- HomeWorks/HomeWork-4.dart
+++ HomeWorks/HomeWork-4.dart
@@ -4,3 +4,31 @@
   4.Parametre olarak girilen kelime ve harf için harfin kelime içinde kaç adet olduğunu gösteren method yazınız.
   ----------
 */
+import 'dart:io';
+
+void main(){
+
+  print("Kelime İçinde Harf Bulma Botumuza Hoş Geldiniz.");
+  print("Lütfen bir kelime giriniz : ");
+  String kelime = stdin.readLineSync()!;
+  print("Lütfen aramak istediğiniz harfi giriniz");
+  String harf = stdin.readLineSync()!;
+  arama(kelime,harf);
+
+}
+
+void arama(String kelime,String harf){
+  int sayac = 0;
+  for(int i =0; i<kelime.length;i++){
+    if(kelime[i]==harf){
+      sayac++;
+    }
+  }
+  if(sayac>0){
+    print("Aradığınız harf kelimenin içinde $sayac adet var.");
+  }
+  else{
+    print("Aradığınız harf kelimenin içinde yok!");
+  }
+
+}
