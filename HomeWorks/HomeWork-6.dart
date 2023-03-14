a9598fe7b61233473c4f6f4bd556654997c521a0
Muharrem GEDİK <39222515+gdkmuharrem@users.noreply.github.com>
2023-03-14 15:09:51 +0300

    Update HomeWork-6.dart

--- HomeWorks/HomeWork-6.dart
+++ HomeWorks/HomeWork-6.dart
@@ -1,6 +1,6 @@
 /*
   HomeWork-6
-  ----------
+  ----------,
   6.Parameter olarak girilen gün sayısına göre maaş hesabı yapan ve elde edilen değeri geri döndüren method yazınız.
       [1 Günde 8 saat çalışılabilir.]
       [Çalışma saat ücreti : 10TL]
@@ -8,3 +8,26 @@
       [160 saat üzeri mesai sayılır.]
   ----------
 */
+import 'dart:io';
+
+void main(){
+
+  print("----------------------------------");
+  print("Maaş Hesabı Botumuza Hoş Geldiniz.");
+  print("----------------------------------");
+  print("Lütfen bu ay kaç gün çalıştığınızı giriniz.");
+  int gun = int.parse(stdin.readLineSync()!);
+  print(maas(gun));
+
+}
+
+int maas(int gun){
+  int sonuc = 0;
+  if(gun>20){
+    sonuc = 20*8*10 + (gun-20)*8*20;
+  }
+  if(gun<20){
+    sonuc = 10*8*gun;
+  }
+  return sonuc;
+}
