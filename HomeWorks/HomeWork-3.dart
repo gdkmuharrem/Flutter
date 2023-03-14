38b812d25a93f36828ed70a67a7c67f479421d9b
Muharrem GEDİK <39222515+gdkmuharrem@users.noreply.github.com>
2023-03-14 14:19:25 +0300

    Update HomeWork-3.dart

--- HomeWorks/HomeWork-3.dart
+++ HomeWorks/HomeWork-3.dart
@@ -4,3 +4,19 @@
   3.Parametre olarak girilen sayının faktoriyel değerini hesaplayıp geri döndüren method yazınız.
   ----------
 */
+import 'dart:io';
+
+void main (){
+
+  print("-------------------------------------------");
+  print("Faktoriyel Hesaplama Botumuza Hoş Geldiniz.");
+  print("-------------------------------------------");
+  print("Lütfen faktoriyelini hesaplamak istediğiniz sayıyı giriniz.");
+  int sayi = int.parse(stdin.readLineSync()!);
+  int faktoryel = 1;
+
+  for(int i = 2;i<=sayi;i++){
+    faktoryel *= i;
+  }
+  print("Sayınızın Faktoriyeli : $faktoryel");
+}
