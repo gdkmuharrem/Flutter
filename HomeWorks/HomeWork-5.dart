dd8984b385925e90a0ea58d9fcd95d0f9b24bc93
Muharrem GEDİK <39222515+gdkmuharrem@users.noreply.github.com>
2023-03-14 14:38:18 +0300

    Update HomeWork-5.dart

--- HomeWorks/HomeWork-5.dart
+++ HomeWorks/HomeWork-5.dart
@@ -2,5 +2,6 @@
   HomeWork-5
   ----------
   5.Parametre olarak girilen kenar sayısına göre iç açılar toplamını hesaplayan sonucu geri gönderen method yazınız.
+      [Formül : n--> kenar sayısı  : (n-2)*180 ]
   ----------
 */
