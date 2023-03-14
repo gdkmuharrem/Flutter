85ba0667b81758a79d4a9715de021be96c780403
Muharrem GEDİK <39222515+gdkmuharrem@users.noreply.github.com>
2023-03-14 15:16:21 +0300

    Update HomeWork-7.dart

--- HomeWorks/HomeWork-7.dart
+++ HomeWorks/HomeWork-7.dart
@@ -6,3 +6,25 @@
       [Kota aşımı sonrası her 1GB 4TL]
   ----------
 */
+import 'dart:io';
+
+void main (){
+  print("------------------------------------------------");
+  print("Kotaya Göre Ücret Hesaplama Botuna Hoş Geldiniz.");
+  print("------------------------------------------------");
+  print("Lütfen bu ay kullandığınız internet kotasını giriniz.");
+  int kota = int.parse(stdin.readLineSync()!);
+  print("Ödemeniz gereken tutar : ${hesap(kota)}");
+
+}
+
+int hesap (int kota){
+  int sonuc = 0 ;
+  if (kota<50){
+    sonuc = 100;
+  }
+  if (kota>50){
+    sonuc = 100+ (kota-50)*4;
+  }
+  return sonuc ;
+}
