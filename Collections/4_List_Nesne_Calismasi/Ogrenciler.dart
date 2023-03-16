0a122a4642a6672914c56ed62800410641be6fe0
Muharrem GEDİK <39222515+gdkmuharrem@users.noreply.github.com>
2023-03-16 03:35:20 +0300

    Create Ogrenciler.dart

---
+++ Collections/4_List_Nesne_Calismasi/Ogrenciler.dart
@@ -0,0 +1,13 @@
+class Ogrenciler{
+
+  //Classımızın değişkenlerini tanımladık.
+  int no;
+  String ad;
+  String sinif;
+
+  //Constructorumuzu tanımladık.
+  Ogrenciler(this.no,this.ad,this.sinif);
+  //Dışarıdan sınıfımıza tanımlama yapmak isteyenlerin artık zorunlu olarak
+// değişkenlerimize uygun değerler yollması gerekmekte.
+
+}
