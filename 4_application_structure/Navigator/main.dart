6e21bae6478ce3d0d8a92c39c7793d82feee4e70
Muharrem GEDİK <39222515+gdkmuharrem@users.noreply.github.com>
2023-10-25 14:38:38 +0300

    main.dart

---
+++ 4_application_structure/Navigator/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:sayfa_gecis_uygulamasi/Kisiler.dart';
+import 'package:sayfa_gecis_uygulamasi/SayfaA.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: AnaSayfa(),
+    );
+  }
+}
+
+class AnaSayfa extends StatefulWidget {
+  @override
+  State<AnaSayfa> createState() => _AnaSayfaState();
+}
+
+class _AnaSayfaState extends State<AnaSayfa> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Anasayfa"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                child: Text("Sayfa A'ya Git"),
+                onPressed: (){
+                  var kisi = Kisiler(isim: "Muharrem",yas: 21,boy: 192,bekarMi: false);
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> SayfaA(kisi: kisi,)));
+                },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
